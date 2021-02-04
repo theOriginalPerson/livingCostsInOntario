96a6e3a23f82e31e2c10307132a7b6c4d4af8014
Dario Karanovi <kleetsonfire@gmail.com>
2021-02-04 09:30:51 -0500

    round

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def living():
     diff = abs(totalCost - totalSalary)
     safeIncluded = abs(diff - safe)
 
-    finalStatement1 = "this is not a good idea, your total salary is " +  str(totalSalary) + "$, and this is less than your total costs of living, which are " + str(totalCost) + "$. Which means that you will be " + str(diff) + "$ in the hole."
-    finalStatement2 = "this is decent, because it still leaves you with enough money to live. Your total salary is " + str(totalSalary) + "$, and your total costs for the month are " + str(totalCost) + "$. Afterwards, you will be left with " + str(diff) + "$"
-    finalStatement3 = "your safety net (how much you want left over each month) is either too high, or you need to cut down on spending. Your remaining balance is only " + str(safeIncluded) + "$, which is less than your safety net of " + str(safe) + "$"
+    finalStatement1 = "this is not a good idea, your total salary is " +  str(round(totalSalary, 2)) + "$, and this is less than your total costs of living, which are " + str(round(totalCost, 2)) + "$. Which means that you will be " + str(round(diff, 2)) + "$ in the hole."
+    finalStatement2 = "this is decent, because it still leaves you with enough money to live. Your total salary is " + str(round(totalSalary, 2)) + "$, and your total costs for the month are " + str(round(totalCost, 2)) + "$. Afterwards, you will be left with " + str(round(diff, 2)) + "$"
+    finalStatement3 = "your safety net (how much you want left over each month) is either too high, or you need to cut down on spending. Your remaining balance is only " + str(round(safeIncluded, 2)) + "$, which is less than your safety net of " + str(round(safe, 2)) + "$"
 
     if totalCost > totalSalary:
         return finalStatement1
