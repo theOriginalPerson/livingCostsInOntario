dcbb9cee8df514cfc1382dc2ed37aa8c520a4b2e
Dario Karanovi <kleetsonfire@gmail.com>
2021-02-03 20:42:01 -0500

    f

--- main.py
+++ main.py
@@ -9,19 +9,35 @@ hoursWorked = "how many hours do you work in one week?   "
 costOfInternet = "what is the cost of your internet?    "
 costOfFood = "how much do you spend on food per month?     "
 costOfLuxuries = "how much do you spend on luxuries (i.e. makeup, clothes, video games, electronics, etc)?      "
+otherCosts = "put any other costs you factor in (in values).    "
 
 safetyNet = "how much money per month do you want to have left over?      "
 
 def living():
-    rent = int(input(costOfRent))
-    wage = int(input(hourlyWage))
+    rent = float(input(costOfRent))
+    wage = float(input(hourlyWage))
     hours = int(input(hoursWorked))
-    internet = int(input(costOfInternet))
-    food = int(input(costOfFood))
-    lux = int(input(costOfLuxuries))
-    safe = int(input(safetyNet))
+    internet = float(input(costOfInternet))
+    food = float(input(costOfFood))
+    lux = float(input(costOfLuxuries))
+    safe = float(input(safetyNet))
+    other = float(input(otherCosts))
 
-    totalSalary = hours * wage * 4 + safe
-    totalCost = rent + internet + food + lux + avgHydro
+    totalSalary = hours * wage * 4
+    totalCost = rent + internet + food + lux + avgHydro + other + safe
 
+    diff = abs(totalCost - totalSalary)
+
+    finalStatement1 = "this is not a good idea, your total salary is " +  str(totalSalary) + "$, and this is less than your total costs of living, which are " + str(totalCost) + "$. Which means that you will be " + str(diff) + "$ in the hole."
+    finalStatement2 = "this is decent, because it still leaves you with enough money to live. Your total salary is " + str(totalSalary) + "$, and your total costs for the month are " + str(totalCost) + "$. Afterwards, you will be left with " + str(diff) + "$"
+
+    if totalCost > totalSalary:
+        return finalStatement1
     
+    elif totalSalary >= totalCost:
+        return finalStatement2
+
+    else: print(ValueError)
+
+x = living()
+print(x)
