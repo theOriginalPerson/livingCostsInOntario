35928b3a780d985ef3e590aca691fd30038c5dfc
Dario Karanovi <kleetsonfire@gmail.com>
2021-02-03 21:33:48 -0500

    final

--- main.py
+++ main.py
@@ -26,18 +26,23 @@ def living():
     other = float(input(otherCosts))
 
     totalSalary = hours * wage * 4
-    totalCost = rent + internet + food + lux + avgHydro + other + safe
+    totalCost = rent + internet + cell + food + lux + avgHydro + other
 
     diff = abs(totalCost - totalSalary)
+    safeIncluded = abs(diff - safe)
 
     finalStatement1 = "this is not a good idea, your total salary is " +  str(totalSalary) + "$, and this is less than your total costs of living, which are " + str(totalCost) + "$. Which means that you will be " + str(diff) + "$ in the hole."
     finalStatement2 = "this is decent, because it still leaves you with enough money to live. Your total salary is " + str(totalSalary) + "$, and your total costs for the month are " + str(totalCost) + "$. Afterwards, you will be left with " + str(diff) + "$"
+    finalStatement3 = "your safety net (how much you want left over each month) is either too high, or you need to cut down on spending. Your remaining balance is only " + str(safeIncluded) + "$, which is less than your safety net of " + str(safe) + "$"
 
     if totalCost > totalSalary:
         return finalStatement1
     
-    elif totalSalary >= totalCost:
+    elif totalCost < totalSalary and safeIncluded > safe:
         return finalStatement2
+
+    elif totalCost < totalSalary and safeIncluded < safe:
+        return finalStatement3 
 
     else: print(ValueError)
 
